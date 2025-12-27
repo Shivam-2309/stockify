7f215d40f31472b5b74ae6e3ba528e4735b7c9e4
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2025-12-27 17:24:35 +0530

    feat: implement header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from "@/components/Header"
 
 const layout = ({children} : {children : React.ReactNode}) => {
   return (
     <main>
         <div className="container py-10">
             {/*Header*/}
+            <Header />
             {children}
         </div>
     </main>
