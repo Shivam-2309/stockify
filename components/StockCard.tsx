435d87e6be75336787d6dba484ee552155ff2aef
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2026-01-07 02:11:11 +0530

    feat: added delete button

--- components/StockCard.tsx
+++ components/StockCard.tsx
@@ -1,14 +1,15 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "./ui/button";
 
 type StockCardProps = {
   stockName: string;
 };
 
-export function StockCard({ stockName }: StockCardProps) {
+export function StockCard({ stockName }: StockCardProps) {  
   return (
     <Card className="border border-gray-200 shadow-sm hover:shadow-md transition-shadow">
       <CardContent className="p-4">
-        <h3 className="font-semibold text-lg text-white">{stockName}</h3>
+        <h3 className="font-semibold text-lg text-white m-2">{stockName}</h3>
       </CardContent>
     </Card>
   );
